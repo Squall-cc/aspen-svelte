c83fc66b4ed26e06255f47ad6352f8dd9e3c8756
a1pl <157911417+a1pl@users.noreply.github.com>
2026-05-04 22:14:56 -0400

    switch seaarch engine again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -64,7 +64,7 @@
   // regex from stack overflow
   linkRegex := /^(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&]*)?)?(#[a-zA-Z0-9!$&'()*+.=\-_~:@\/?]*)?)$/i
 
-  searxUrl := 'https://searx.namejeff.xyz/search?q='
+  searxUrl := 'https://www.metacrawler.com/serp?q='
 
   let searchInput = $state('')
 
